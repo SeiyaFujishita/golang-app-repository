44a5ad10913a3ebce0e0fa5b3fc2537d713a8ff1
SeiyaFujishita <hakugen07@gmail.com>
2023-06-06 08:16:34 +0900

    🎨 learn map

--- main.go
+++ main.go
@@ -3,5 +3,18 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Printf("first commit!\n")
+	m := make([]map[string]int, 0)
+	newMap1 := make(map[string]int)
+	newMap2 := make(map[string]int)
+	newMap3 := make(map[string]int)
+	newMap1["key"] = 1
+	newMap2["key"] = 2
+	newMap3["key"] = 3
+	m = append(m, newMap1)
+	m = append(m, newMap2)
+	m = append(m, newMap3)
+
+	for key, value := range m {
+		fmt.Println("Key:", key, "Value:", value)
+	}
 }
